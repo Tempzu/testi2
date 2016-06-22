8bdbbac5813ebdb865e441a047316e8cc818a06f
Tempzu <ttlaakso@gmail.com>
2016-06-22 22:20:00 +0300

    Update emails.js

--- js/services/emails.js
+++ js/services/emails.js
@@ -1,5 +1,5 @@
 app.factory('emails', ['$http', function($http) {
-  return $http.get('https://s3.amazonaws.com/codecademy-content/courses/ltp4/emails-api/events.json')
+  return $http.get('https://s3.amazonaws.com/codecademy-content/courses/ltp4/emails-api/emails.json')
             .success(function(data) {
               return data;
             })
