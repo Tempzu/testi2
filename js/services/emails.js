9f145406b1aeb30022ab5a2b3487bb2b6b926830
Tempzu <ttlaakso@gmail.com>
2016-06-22 21:46:38 +0300

    Update emails.js

--- js/services/emails.js
+++ js/services/emails.js
@@ -1,5 +1,5 @@
 app.factory('emails', ['$http', function($http) {
-  return $http.get('https://tempzu.github.io/testi3/')
+  return $http.get('https://tempzu.github.io/testi3-emails.json/')
             .success(function(data) {
               return data;
             })
